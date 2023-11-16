5542b01a416f0017f6a94ecc61ec67fd611c2d51
astrod <44481578+lunastrod@users.noreply.github.com>
2023-11-16 15:00:43 +0100

    p4

--- adjustTheta.m
+++ adjustTheta.m
@@ -1,17 +1,31 @@
 clear;  % Clear workspace
 clc;    % Clear command window
 close all;  % Close all figures
+load_system("solucion.slx");
 
-Ts=0.1
-refx=5
-refy=1
-load_system("solucion.slx")
-out=sim("solucion","StopTime",num2str(10))
+% Parameters
+Ts = 100e-3;
+numSimulations = 30;
 
-out.E_d;
-out.E_theta;
-out.V;
-out.W;
-out.x;
-out.y;
-out.theta;
+% Initialize matrices to store data
+inputMatrix = [];
+outputMatrix = [];
+
+% Loop for simulations
+for simIdx = 1:numSimulations
+    disp(simIdx)
+    % Generate random setpoints within the 10x10 environment
+    refx = rand() * 10;
+    refy = rand() * 10;
+
+    % Run simulation
+    out = sim("solucion", "StopTime", num2str(10));
+
+    % Accumulate input and output values
+    inputMatrix = [inputMatrix; out.E_d, out.E_theta];
+    outputMatrix = [outputMatrix; out.V, out.W];
+end
+
+% Save matrices
+save('inputMatrix.mat', 'inputMatrix');
+save('outputMatrix.mat', 'outputMatrix');
